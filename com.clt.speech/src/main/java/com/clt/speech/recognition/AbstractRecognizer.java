de7158eae0c4a7d483f9b7411b0a563cd0a6c8fd
Timo Baumann <baumann@informatik.uni-hamburg.de>
2018-02-01 18:13:46 -0500

    simplify AbstractRecognizer

--- com.clt.speech/src/main/java/com/clt/speech/recognition/AbstractRecognizer.java
+++ com.clt.speech/src/main/java/com/clt/speech/recognition/AbstractRecognizer.java
@@ -1,22 +1,13 @@
 
 package com.clt.speech.recognition;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
@@ -24,9 +15,6 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import com.clt.event.ProgressEvent;
-import com.clt.event.ProgressListener;
-import com.clt.io.PreprocessingReader;
 import com.clt.properties.Property;
 import com.clt.script.Environment;
 import com.clt.script.debug.Debugger;
@@ -43,8 +31,6 @@ import com.clt.speech.G2P;
 import com.clt.speech.Language;
 import com.clt.speech.SpeechException;
 import com.clt.srgf.Grammar;
-import com.clt.srgf.GrammarContext;
-import com.clt.util.DiskTools;
 
 /**
  * @author Daniel Bobbert
@@ -52,28 +38,25 @@ import com.clt.util.DiskTools;
  */
 public abstract class AbstractRecognizer implements Recognizer, G2P {
 
-    private Collection<RecognizerListener> listeners = new ArrayList<RecognizerListener>();
+    private final Collection<RecognizerListener> listeners = new ArrayList<RecognizerListener>();
 
     private boolean active = false;
     private boolean live = true;
-
-    private Domain internalDomain = null;
-    private GrammarContext internalGrammarContext = null;
 
     private Map<String, Property<?>> additionalParameters
             = new HashMap<String, Property<?>>();
 
     public void addRecognizerListener(RecognizerListener l) {
 
-        synchronized (this.listeners) {
-            this.listeners.add(l);
+        synchronized (listeners) {
+            listeners.add(l);
         }
     }
 
     public void removeRecognizerListener(RecognizerListener l) {
 
-        synchronized (this.listeners) {
-            this.listeners.remove(l);
+        synchronized (listeners) {
+            listeners.remove(l);
         }
     }
 
@@ -87,15 +70,10 @@ public abstract class AbstractRecognizer implements Recognizer, G2P {
         this.fireRecognizerEvent(new RecognizerEvent(this, result));
     }
 
-    protected void fireRecognizerWarning(String message) {
-
-        this.fireRecognizerEvent(new RecognizerEvent(this, message));
-    }
-
     protected void fireRecognizerEvent(RecognizerEvent evt) {
 
-        synchronized (this.listeners) {
-            for (RecognizerListener listener : this.listeners) {
+        synchronized (listeners) {
+            for (RecognizerListener listener : listeners) {
                 listener.recognizerStateChanged(evt);
             }
         }
@@ -155,9 +133,6 @@ public abstract class AbstractRecognizer implements Recognizer, G2P {
 
             try {
                 result = this.startImpl();
-            } catch (Throwable e) {
-                e.printStackTrace();
-                throw e;
             } finally {
                 synchronized (this) {
                     this.active = false;
@@ -283,226 +258,13 @@ public abstract class AbstractRecognizer implements Recognizer, G2P {
     }
 
     /**
-     * Stops the speech recognizer. This method should do whatever is needed to
+     * Stops the speech recognizer (and makes it return null).
+     * This method should do whatever is needed to
      * terminate the recognizer and perform any necessary cleanup.
      *
      * @throws SpeechException
      */
     protected abstract void stopImpl() throws SpeechException;
-
-    public Collection<RecognitionContext> loadGrammars(File grammarDir, ProgressListener l) throws SpeechException, GrammarException {
-        return this.loadGrammars(grammarDir, l, false);
-    }
-
-    public Collection<RecognitionContext> loadGrammars(File grammarDir, ProgressListener l, boolean forceRecompile)
-            throws SpeechException, GrammarException {
-        Domain[] ds = this.getDomains();
-        for (int i = 0; i < ds.length; i++) {
-            ds[i].dispose();
-        }
-
-        Collection<ContextCreator> contextCreators = new ArrayList<ContextCreator>();
-
-        ProgressEvent evt = null;
-        File domains[] = grammarDir.listFiles(AbstractRecognizer.grammarFilter);
-        Map<Domain, Map<String, RecognitionContext.Info>> contextCache;
-        if (domains != null) {
-            if (forceRecompile) {
-                contextCache
-                        = new HashMap<Domain, Map<String, RecognitionContext.Info>>();
-            } else {
-                try {
-                    if (l != null) {
-                        evt
-                                = new ProgressEvent(this, "Loading context database...", 0, 0, 0);
-                        l.progressChanged(evt);
-                    }
-                    contextCache = this.loadContextCache(grammarDir);
-                } catch (Exception exn) {
-                    contextCache
-                            = new HashMap<Domain, Map<String, RecognitionContext.Info>>();
-                }
-            }
-
-            if (l != null) {
-                evt
-                        = new ProgressEvent(this, "Loading grammars...", 0, DiskTools
-                                .countFiles(
-                                        grammarDir, AbstractRecognizer.grammarFilter) * 2, 0);
-                l.progressChanged(evt);
-            }
-
-            for (int i = 0; i < domains.length; i++) {
-                if (domains[i].isDirectory()) {
-                    GrammarContext grammarContext
-                            = new GrammarContext(domains[i].getName());
-                    Domain domain = this.findDomain(domains[i].getName(), true);
-                    Map<String, RecognitionContext.Info> cache = contextCache.get(domain);
-                    if (cache == null) {
-                        cache = new HashMap<String, RecognitionContext.Info>();
-                    }
-                    this.loadGrammars(domains[i], "", contextCreators, domain, cache,
-                            grammarContext, l,
-                            evt);
-                    grammarContext.optimize(false);
-                }
-            }
-        }
-
-        List<RecognitionContext> contexts = new ArrayList<RecognitionContext>(
-                contextCreators.size());
-        for (ContextCreator creator : contextCreators) {
-            evt.setMessage("Creating context " + creator.name);
-            evt.setCurrent(evt.getCurrent() + 1);
-            if (l != null) {
-                l.progressChanged(evt);
-            }
-            RecognitionContext context = creator.create();
-            if (context != null) {
-                contexts.add(context);
-            }
-        }
-
-        try {
-            if (l != null) {
-                evt = new ProgressEvent(this, "Saving context database...", 0, 1, 1);
-                l.progressChanged(evt);
-            }
-            this.saveContextCache(contexts, grammarDir);
-        } catch (Exception ignore) {
-        }
-
-        return contexts;
-    }
-
-    private void loadGrammars(File grammarDir, String prefix,
-            Collection<ContextCreator> contexts,
-            Domain domain, Map<String, RecognitionContext.Info> contextCache,
-            GrammarContext grammarContext, ProgressListener l, ProgressEvent evt)
-            throws RecognizerException, GrammarException {
-
-        File files[] = grammarDir.listFiles(AbstractRecognizer.grammarFilter);
-        if (files != null) {
-            for (int i = 0; i < files.length; i++) {
-                if (files[i].isDirectory()) {
-                    this.loadGrammars(files[i], prefix + files[i].getName() + ".",
-                            contexts, domain,
-                            contextCache, grammarContext, l, evt);
-                } else {
-                    String context = files[i].getName();
-                    int ending = context.lastIndexOf('.');
-                    if (ending < 0) {
-                        ending = context.length();
-                    }
-                    context = context.substring(0, ending);
-
-                    try {
-                        if (l != null) {
-                            evt.setMessage("Loading " + domain.getName() + ": " + prefix
-                                    + context);
-                            l.progressChanged(evt);
-                        }
-
-                        Reader r = new PreprocessingReader(files[i]);
-                        try {
-                            Grammar g = Grammar.create(r, this.getEnvironment());
-                            if (g.getName() == null) {
-                                if ((context.indexOf(' ') >= 0) || (context.indexOf('.') >= 0)) {
-                                    throw new RecognizerException(
-                                            "Grammar names may not contain spaces or '.'");
-                                }
-
-                                g.setName(prefix + context);
-                            }
-
-                            if (grammarContext != null) {
-                                grammarContext.add(g);
-                            } else {
-                                g.optimizeInPlace(false);
-                            }
-
-                            RecognitionContext.Info info = contextCache.get(g.getName());
-                            Set<File> includedFiles
-                                    = PreprocessingReader.getIncludedFiles(files[i]);
-                            long timestamp = files[i].lastModified();
-                            for (File f : includedFiles) {
-                                timestamp = Math.max(timestamp, f.lastModified());
-                            }
-
-                            contexts.add(new ContextCreator(g.getName(), g, domain,
-                                    timestamp, info));
-                            if (l != null) {
-                                evt.setCurrent(evt.getCurrent() + 1);
-                                l.progressChanged(evt);
-                            }
-                        } finally {
-                            r.close();
-                        }
-                    } catch (Exception exn) {
-                        throw new GrammarException(files[i], exn);
-                    }
-                }
-            }
-        }
-    }
-
-    public void loadInternalGrammars(String encoding)
-            throws Exception {
-
-        InputStream is = null;
-        try {
-            is = this.getClass().getClassLoader().getResourceAsStream("grammars.txt");
-        } catch (Exception exn) {
-        }
-
-        if (is != null) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(is));
-            Map<String, Grammar> grammars = new HashMap<String, Grammar>();
-
-            String grammarName;
-            while ((grammarName = in.readLine()) != null) {
-                grammarName = grammarName.trim();
-                if ((grammarName.length() > 0) && !grammarName.startsWith("#")) {
-                    InputStream gStream
-                            = this.getClass().getClassLoader().getResourceAsStream(
-                                    "grammars/" + grammarName + ".bnf");
-                    if (gStream == null) {
-                        throw new IOException("Internal grammar " + grammarName
-                                + " not found");
-                    }
-
-                    if (this.internalDomain == null) {
-                        this.internalDomain = this.createDomain("Internal");
-                    }
-
-                    if (this.internalGrammarContext == null) {
-                        this.internalGrammarContext = new GrammarContext();
-                    }
-
-                    Reader r
-                            = new InputStreamReader(new BufferedInputStream(gStream), encoding);
-                    Grammar g = Grammar.create(r, this.getEnvironment());
-                    r.close();
-                    if (g.getName() == null) {
-                        g.setName(grammarName);
-                    }
-                    this.internalGrammarContext.add(g);
-
-                    grammars.put(g.getName(), g);
-                }
-            }
-
-            for (String name : grammars.keySet()) {
-                RecognitionContext ctx
-                        = this.createContext(name, grammars.get(name), this.internalDomain,
-                                System.currentTimeMillis());
-                if (ctx != null) {
-                    this.internalDomain.addContext(ctx);
-                }
-            }
-        }
-
-    }
 
     protected Environment getEnvironment() {
 
@@ -612,11 +374,6 @@ public abstract class AbstractRecognizer implements Recognizer, G2P {
         }
     }
 
-    protected void registerAdditionalParameter(Property<?> parameter) {
-
-        this.additionalParameters.put(parameter.getID(), parameter);
-    }
-
     public final Property<?>[] getParameters() {
 
         return this.additionalParameters.values().toArray(
@@ -690,17 +447,6 @@ public abstract class AbstractRecognizer implements Recognizer, G2P {
         }
     }
 
-    /**
-     * @return a Map&lt;Domain, Map&lt;String, RecognitionContext.Info&gt;&gt;
-     */
-    protected abstract Map<Domain, Map<String, RecognitionContext.Info>> loadContextCache(
-            File grammarDirectory)
-            throws SpeechException, IOException;
-
-    /**
-     * @param contexts A collection of {@link RecognitionContext}s
-     */
-    protected abstract void saveContextCache(Collection<RecognitionContext> contexts, File grammarDirectory) throws SpeechException, IOException;
 
     protected abstract RecognitionContext createContext(String name, Grammar g, Domain domain, long timestamp) throws SpeechException;
 
@@ -725,86 +471,4 @@ public abstract class AbstractRecognizer implements Recognizer, G2P {
         return transcriptions;
     }
 
-    private static FileFilter grammarFilter = new FileFilter() {
-        public boolean accept(File path) {
-            // skip files and directories starting with "."
-            // accept files ending in .gram or .bnf or .srgf
-            String name = path.getName();
-            return !name.startsWith(".")
-                    && (path.isDirectory() || (name.endsWith(".bnf")
-                    || name.endsWith(".gram") || name.endsWith(".srgf")));
-        }
-    };
-
-    public static class GrammarException
-            extends Exception {
-
-        File grammar;
-        Exception exn;
-
-        public GrammarException(File grammar, Exception exn) {
-
-            super("Error while reading grammar file " + grammar + ":\n"
-                    + exn.toString());
-            this.grammar = grammar;
-            this.exn = exn;
-        }
-
-        @Override
-        public void printStackTrace() {
-
-            this.exn.printStackTrace();
-        }
-
-        @Override
-        public void printStackTrace(PrintStream s) {
-
-            this.exn.printStackTrace(s);
-        }
-
-        @Override
-        public String toString() {
-
-            return this.getLocalizedMessage();
-        }
-    }
-
-    private class ContextCreator {
-
-        private String name;
-        private Grammar g;
-        private Domain domain;
-        private long timestamp;
-        private RecognitionContext.Info info;
-
-        public ContextCreator(String name, Grammar g, Domain domain,
-                long timestamp,
-                RecognitionContext.Info info) {
-
-            this.name = name;
-            this.g = g;
-            this.domain = domain;
-            this.timestamp = timestamp;
-            this.info = info;
-        }
-
-        public RecognitionContext create()
-                throws SpeechException {
-
-            boolean compile
-                    = (this.info == null) || (this.info.getTimestamp() < this.timestamp);
-            RecognitionContext ctx;
-            if (compile) {
-                ctx
-                        = AbstractRecognizer.this.createContext(this.g.getName(), this.g,
-                                this.domain, this.timestamp);
-            } else {
-                ctx = this.info.createContext(this.g, this.domain);
-            }
-            if (ctx != null) {
-                this.domain.addContext(ctx);
-            }
-            return ctx;
-        }
-    }
 }
