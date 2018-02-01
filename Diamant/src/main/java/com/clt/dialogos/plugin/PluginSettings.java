013b2cf49c79f87413dc161298481a9bc248823c
Timo Baumann <baumann@informatik.uni-hamburg.de>
2018-02-01 18:14:51 -0500

    enable plugins to read/store more complex XML beyond name-value pairs

--- Diamant/src/main/java/com/clt/dialogos/plugin/PluginSettings.java
+++ Diamant/src/main/java/com/clt/dialogos/plugin/PluginSettings.java
@@ -1,6 +1,7 @@
 package com.clt.dialogos.plugin;
 
 import java.awt.Component;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -62,17 +63,30 @@ public abstract class PluginSettings {
                             }
                         });
                     } else {
-                        PluginSettings.this.readAttribute(r, n, atts
-                                .getValue("value"), uid_map);
-                    }
-                }
-            }
-        });
-    }
+            PluginSettings.this.readAttribute(r, n, atts
+                .getValue("value"), uid_map);
+          }
+        } else {
+          readOtherXML(r, name, atts, uid_map);
+        }
+      }
+    });
+  }
 
-    protected abstract void readAttribute(XMLReader r, String name,
-            String value, IdMap uid_map)
-            throws SAXException;
+
+  /**
+   * use this operation to read previously stored name-value pairs
+   */
+  protected abstract void readAttribute(XMLReader r, String name,
+      String value, IdMap uid_map)
+      throws SAXException;
+
+  /**
+   * override this operation if you need to deal with more than just name-value pairs
+   */
+  protected void readOtherXML(XMLReader r, String name, Attributes atts, IdMap uid_map) throws SAXException {
+    throw new RuntimeException(new IOException("weird xml, did not expect " + name + " here."));
+  }
 
     /**
      * Creates an editor component to configure the plug-in.
