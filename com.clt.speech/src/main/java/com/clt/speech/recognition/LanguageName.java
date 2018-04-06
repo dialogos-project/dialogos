e1e5c0e4c5952282b90692d3f379025608a1b845
Timo Baumann <baumann@informatik.uni-hamburg.de>
2018-04-06 18:07:35 -0400

    once more fix bug #8. let's see how often it will crop up again...

--- com.clt.speech/src/main/java/com/clt/speech/recognition/LanguageName.java
+++ com.clt.speech/src/main/java/com/clt/speech/recognition/LanguageName.java
@@ -1,17 +1,3 @@
-/*
- * @(#)LanguageName.java
- * Created on 03.04.2007 by dabo
- *
- * Copyright (c) CLT Sprachtechnologie GmbH.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of CLT Sprachtechnologie GmbH ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with CLT Sprachtechnologie GmbH.
- */
-
 package com.clt.speech.recognition;
 
 import com.clt.speech.Language;
