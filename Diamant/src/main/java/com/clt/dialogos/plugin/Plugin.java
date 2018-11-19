47385ab0ede9bcc60c561fefe795c2f392f8330d
Alexander Koller <koller@ling.uni-potsdam.de>
2018-11-19 15:57:29 +0100

    added some javadoc for Plugin

--- Diamant/src/main/java/com/clt/dialogos/plugin/Plugin.java
+++ Diamant/src/main/java/com/clt/dialogos/plugin/Plugin.java
@@ -1,10 +1,14 @@
 package com.clt.dialogos.plugin;
 
+import com.clt.diamant.graph.Node;
 import javax.swing.*;
 
 /**
- * The interface for DialogOS plugins. Plugin classes should load instantly, any
- * costly operations should be deferred to {@link #initialize()}.
+ * The interface for DialogOS plugins. A plugin provides a set of custom node
+ * types, which can then be used in DialogOS dialogs.<p>
+ *
+ * See the <a href="https://github.com/dialogos-project/dialogos/wiki/Plugins">
+ * Wiki page on plugins</a> for more details.
  *
  * @author dabo, 2mfriedr
  *
@@ -13,44 +17,65 @@ public interface Plugin {
 
     /**
      * A unique ID to identify this plugin, e.g. com.example.myplugin
+     * @return 
      */
     String getId();
 
     /**
-     * A user readable name for this plugin.
+     * A user readable name for this plugin. This name is displayed
+     * in the plugin's entry in the "Dialog" menu of DialogOS.
+     * 
+     * @return 
      */
     String getName();
 
     /**
-     * The icon for this plugin, for the settings screen.
+     * An icon for this plugin. This icon is displayed alongside
+     * the name returned by {@link #getName() } in the plugin list
+     * of the DialogOS settings window, which appears when any of
+     * the plugins or devices in the "Dialog" menu is selected.
+     * 
+     * @return 
      */
     Icon getIcon();
 
     /**
-     * The version string, for the settings screen.
+     * The version string, for the settings screen. See {@link #getIcon() }.
      *
+     * @return 
      */
     String getVersion();
 
     /**
-     * This method is called when a plugin is loaded. Plugins can override it
-     * e.g. to register their node types or to perform other costly operations.
-     * An empty implementation is provided by default.
+     * This method is called when a plugin is loaded. This usually happens when
+     * DialogOS is started, i.e. on the startup screen.<p>
+     *
+     * Plugins should load instantly; so the constructor of a plugin should not
+     * perform any costly operations. Instead, these operations should be
+     * deferred to the initialize method.<p>
+     * 
+     * The most important function of {@link #initialize() } is to register
+     * the node types of the plugin with DialogOS, using {@link Node#registerNodeTypes(java.lang.Object, java.util.Collection) }.<p>
+     *
+     * The default implementation of initialize does nothing.
      */
     default void initialize() {
     }
 
-  /**
-   * This method is called when a plugin should terminate. It should be used to clean up any resources. An empty
-   * implementation is provided by default.
-   */
+    /**
+     * This method is called when a plugin should terminate. It should be used
+     * to clean up any resources. An empty implementation is provided by
+     * default.
+     */
     default void terminate() {
     }
 
-  /**
-   * This method is called later from the document. Plugins need to return their settings (including a settings GUI
-   * component and reading and writing from XML).
-   * @return the plugin settings
-   */
+    /**
+     * This method is called later from the document. Plugins need to return
+     * their settings (including a settings GUI component and reading and
+     * writing from XML).
+     *
+     * @return the plugin settings
+     */
     PluginSettings createDefaultSettings();
 }
