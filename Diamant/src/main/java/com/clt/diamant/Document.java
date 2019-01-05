e37271e109445e809bf7e3a19eb8252a5f50770d
Alexander Koller <koller@ling.uni-potsdam.de>
2019-01-05 07:53:09 +0100

    can save Document without changing window title

--- Diamant/src/main/java/com/clt/diamant/Document.java
+++ Diamant/src/main/java/com/clt/diamant/Document.java
@@ -80,11 +80,39 @@ public abstract class Document {
         out.println();
     }
 
+    /**
+     * Saves this Document to a file, clears the dirty status of the Document,
+     * and sets the window title to the new filename.
+     *
+     * @param parent
+     * @param f
+     * @param l
+     * @throws Exception
+     */
     public void save(Component parent, File f, ProgressListener l) throws Exception {
+        save(parent, f, true, l);
+    }
 
-        l.progressChanged(new ProgressEvent(this, Resources.getString("Validating")
-                + "...", 0, 0,
-                0));
+    /**
+     * Saves this Document to a file. If isFullSave is set to true, this will
+     * also clear the document's dirty status and set the window title to the
+     * new filename. This is useful e.g. for the File -> Save menu item and its
+     * relatives. Alternatively, if you only want to save a copy of the Document
+     * to a file without a noticeable change to the user, use this method with
+     * isFullSave=false.
+     *
+     * @param parent
+     * @param f
+     * @param isFullSave
+     * @param l
+     * @throws Exception
+     */
+    public void save(Component parent, File f, boolean isFullSave, ProgressListener l) throws Exception {
+
+        l.progressChanged(new ProgressEvent(
+                this,
+                Resources.getString("Validating") + "...",
+                0, 0, 0));
 
         Collection<SearchResult> errors = new LinkedList<SearchResult>();
 
@@ -101,26 +129,28 @@ public abstract class Document {
             throw new FileSaveException(f, errors);
         }
 
-        l.progressChanged(new ProgressEvent(this, Resources.getString("Writing")
-                + "...", 0, 1, 0));
+        l.progressChanged(new ProgressEvent(this, Resources.getString("Writing") + "...", 0, 1, 0));
 
         XMLWriter out = null;
         File tmpFile = File.createTempFile("Save", ".xml", f.getParentFile());
         try {
+            // write the document into a temporary file
             out = new XMLWriter(tmpFile);
             this.writeHeader(out);
 
             IdMap uid_map = new IdMap(true);
             this.write(out, uid_map);
 
-            l.progressChanged(new ProgressEvent(this, Resources.getString("Writing")
-                    + "...", 0, 1,
-                    1));
+            l.progressChanged(new ProgressEvent(
+                    this,
+                    Resources.getString("Writing") + "...",
+                    0, 1, 1));
 
             out.close();
             out = null;
-            this.setDirty(false);
 
+            // if everything worked okay, rename the temporary
+            // file to the intended name
             boolean success = true;
             try {
                 if (f.exists()) {
@@ -141,9 +171,13 @@ public abstract class Document {
                     Resources.format("CouldNotSaveFile", f.getName()),
                     Resources.format("SavedFileAs", this.file.getName())});
             }
-            this.setTitle(this.file.getName());
-            // DiskTools.setFileCreator(file, "WOzS");
-            // DiskTools.setFileType(file, "TEXT");
+
+            // if isFullSave is true, clear the document's dirty status
+            // and change the window title
+            if (isFullSave) {
+                setDirty(false);
+                setTitle(this.file.getName());
+            }
         } finally {
             if (out != null) {
                 out.close();
