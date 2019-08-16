7d521d1899ecd1dbb915519a8553d664abcde418
Timo Baumann <baumann@informatik.uni-hamburg.de>
2019-08-16 13:50:40 +0200

    release 2.1.2

--- Diamant/src/main/java/com/clt/diamant/Version.java
+++ Diamant/src/main/java/com/clt/diamant/Version.java
@@ -23,7 +23,7 @@ public class Version {
     public static final int MINOR_VERSION = 1;
     public static final int BUGFIX_VERSION = 2;
 //    public static final String NONRELEASE = "beta1";
-    public static final boolean IS_NONRELEASE = true;
+    public static final boolean IS_NONRELEASE = false;
 
     public static String getShortGitRevision() {
         InputStream is = Version.class.getResourceAsStream("/VERSION");
