ffff22802d8555f9fde3f2dec17406b4ab7d0b3a
Timo Baumann <baumann@informatik.uni-hamburg.de>
2024-11-22 09:17:06 +0100

    partial fix for https://github.com/dialogos-project/dialogos/issues/215 mary now stores and loads voice defaults

--- plugins/DialogOS_MaryTTSPlugin/src/main/java/de/saar/coli/dialogos/marytts/plugin/Settings.java
+++ plugins/DialogOS_MaryTTSPlugin/src/main/java/de/saar/coli/dialogos/marytts/plugin/Settings.java
@@ -16,6 +16,7 @@ import javax.swing.JPanel;
 import com.clt.dialogos.plugin.PluginRuntime;
 import com.clt.dialogos.plugin.PluginSettings;
 import com.clt.diamant.IdMap;
+import com.clt.diamant.graph.Graph;
 import com.clt.diamant.graph.Node;
 import com.clt.gui.GUI;
 import com.clt.gui.OptionPane;
@@ -37,9 +38,16 @@ import com.clt.xml.XMLWriter;
  */
 public class Settings extends PluginSettings {
 
+    private static final String DEFAULT_VOICE = "defaultVoice";
     private final EnumProperty<VoiceName> defaultVoice;
+    private static final String DEFAULT_SPEED = "defaultTempo";
+    private static final int DEFAULT_SPEED_VALUE = 100;
     private final IntegerProperty speed;
+    private static final String DEFAULT_PITCH = "defaultRegister";
+    private static final int DEFAULT_PITCH_VALUE = 0;
     private final IntegerProperty pitch;
+    private static final String DEFAULT_VOLUME = "defaultLoudness";
+    private static final int DEFAULT_VOLUME_VALUE = 80;
     private final IntegerProperty volume;
 
     public Settings() {
@@ -48,16 +56,13 @@ public class Settings extends PluginSettings {
         this.defaultVoice = new DefaultEnumProperty<VoiceName>("voice",
                         Resources.getString("DefaultVoice"), null, voices
                         .toArray(new VoiceName[voices.size()]), voices.get(0)) {
-
             @Override
             public String getName() {
-
                 return Resources.getString("DefaultVoice");
             }
 
             @Override
             public void setValueFromString(String value) {
-
                 for (VoiceName n : this.getPossibleValues()) {
                     if (n.toString().equals(value) || n.getName().equals(value)) {
                         this.setValue(n);
@@ -67,38 +72,31 @@ public class Settings extends PluginSettings {
             }
         };
 
-        this.speed = new DefaultIntegerProperty("speed", Resources.getString("Speed"), null, 0, 200, 100) {
+        this.speed = new DefaultIntegerProperty("speed", Resources.getString("Speed"), null, 0, 200, DEFAULT_SPEED_VALUE) {
             @Override
             public String getName() {
                 return Resources.getString("Speed");
             }
         };
 
-        this.pitch
-                = new DefaultIntegerProperty("pitch", Resources.getString("Pitch"), null,
-                        -200, 200, 0) {
-
+        this.pitch = new DefaultIntegerProperty("pitch", Resources.getString("Pitch"), null,
+                        -200, 200, DEFAULT_PITCH_VALUE) {
             @Override
             public String getName() {
-
                 return Resources.getString("Pitch");
             }
         };
 
-        this.volume
-                = new DefaultIntegerProperty("volume", Resources.getString("Volume"), null,
-                        0, 100,
-                        80) {
-
+        this.volume = new DefaultIntegerProperty("volume", Resources.getString("Volume"), null,
+                        0, 100, DEFAULT_VOLUME_VALUE) {
             @Override
             public String getName() {
-
                 return Resources.getString("Volume");
             }
         };
 
         if (!voices.isEmpty()) {
-            this.defaultVoice.setValue(voices.iterator().next());
+            this.defaultVoice.setValue(voices.get(0));
         }
 
     }
@@ -108,12 +106,10 @@ public class Settings extends PluginSettings {
     }
 
     public int getSpeed() {
-
         return this.speed.getValue();
     }
 
     public int getPitch() {
-
         return this.pitch.getValue();
     }
 
@@ -132,7 +128,6 @@ public class Settings extends PluginSettings {
         p.add(new PropertySet<Property<?>>(this.defaultVoice,
                 this.speed, this.pitch, this.volume).createPropertyPanel(false),
                 BorderLayout.NORTH);
-
         JPanel bottom = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
         final JButton tryPrompt = new JButton(Resources.getString("Try"));
@@ -144,12 +139,34 @@ public class Settings extends PluginSettings {
 
     @Override
     public void writeAttributes(XMLWriter out, IdMap uidMap) {
-        // nothing to be written
+        if (!this.getDefaultVoice().equals(Plugin.getAvailableVoices().get(0))) {
+            Graph.printAtt(out, DEFAULT_VOICE, this.getDefaultVoice().toString());
+        }
+        if (this.getSpeed() != DEFAULT_SPEED_VALUE) {
+            Graph.printAtt(out, DEFAULT_SPEED, this.getSpeed());
+        }
+        if (this.getVolume() != DEFAULT_VOLUME_VALUE) {
+            Graph.printAtt(out, DEFAULT_PITCH, this.getPitch());
+        }
+        if (this.getVolume() != DEFAULT_VOLUME_VALUE) {
+            Graph.printAtt(out, DEFAULT_VOLUME, this.getVolume());
+        }
     }
 
     @Override
     protected void readAttribute(XMLReader r, String name, String value, IdMap uidMap) {
-        // nothing to be read
+        if (name.equals(DEFAULT_VOICE)) {
+            this.defaultVoice.setValueFromString(value);
+        }
+        if (name.equals(DEFAULT_SPEED)) {
+            this.speed.setValueFromString(value);
+        }
+        if (name.equals(DEFAULT_PITCH)) {
+            this.pitch.setValueFromString(value);
+        }
+        if (name.equals(DEFAULT_VOLUME)) {
+            this.volume.setValueFromString(value);
+        }
     }
 
     @Override
